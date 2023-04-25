cf880f8c018b744ee8983165b349206b0e706848
Rahul Sharma <rahulsh3105@gmail.com>
2023-04-25 11:23:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-//your JS code here. If required.
-const form = document.querySelector("form");
-const fontSizeInput = document.querySelector("#fontsize");
-const fontColorInput = document.querySelector("#fontcolor");
-
-// Load previously saved preferences from local storage
-const savedFontSize = localStorage.getItem("preferredFontSize");
-const savedFontColor = localStorage.getItem("preferredFontColor");
-if (savedFontSize) {
-  document.documentElement.style.setProperty("--fontsize", savedFontSize);
-  fontSizeInput.value = savedFontSize.slice(0, -2); // remove "px" suffix
-}
-if (savedFontColor) {
-  document.documentElement.style.setProperty("--fontcolor", savedFontColor);
-  fontColorInput.value = savedFontColor;
-}
-
-// Handle form submit
-form.addEventListener("submit", (event) => {
+const form = document.querySelector('form');
+const fontSizeInput = document.getElementById('fontsize');
+const fontColorInput = document.getElementById('fontcolor');
+// Get the current font size and color from the CSS variables
+const currentFontSize = getComputedStyle(document.body).getPropertyValue('--fontsize');
+const currentFontColor = getComputedStyle(document.body).getPropertyValue('--fontcolor');
+// Set the form input values to the current font size and color
+fontSizeInput.value = parseInt(currentFontSize);
+fontColorInput.value = currentFontColor;
+// Add an event listener to the form
+form.addEventListener('submit', function(event) {
+  // Prevent the form from submitting and reloading the page
   event.preventDefault();
-  const fontSize = fontSizeInput.value + "px";
-  const fontColor = fontColorInput.value;
-
-  // Update page styles
-  document.documentElement.style.setProperty("--fontsize", fontSize);
-  document.documentElement.style.setProperty("--fontcolor", fontColor);
-
-  // Store new preferences in local storage
-  localStorage.setItem("preferredFontSize", fontSize);
-  localStorage.setItem("preferredFontColor", fontColor);
+  // Set the new font size and color CSS variables
+  document.documentElement.style.setProperty('--fontsize', fontSizeInput.value + 'px');
+  document.documentElement.style.setProperty('--fontcolor', fontColorInput.value);
+  // Store the font size and color values in cookies
+  document.cookie = 'fontsize=' + fontSizeInput.value;
+  document.cookie = 'fontcolor=' + fontColorInput.value;
+});
+// Read the font size and color values from cookies and apply them to the page
+document.cookie.split(';').forEach(function(cookie) {
+  const [name, value] = cookie.split('=');
+  if (name.trim() === 'fontsize') {
+    document.documentElement.style.setProperty('--fontsize', value + 'px');
+    fontSizeInput.value = parseInt(value);
+  } else if (name.trim() === 'fontcolor') {
+    document.documentElement.style.setProperty('--fontcolor', value);
+    fontColorInput.value = value;
+  }
 });
